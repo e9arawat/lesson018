293475a164f590f5cd77e8444ab4cb9453fb544a
e9arawat <153479261+e9arawat@users.noreply.github.com>
2024-01-06 19:14:16 +0530

    Completed

--- answer.py
+++ answer.py
@@ -36,6 +36,7 @@ def compound_interest_with_payments(principal, payment, term, rate, end_of_perio
         principal += ans
     principal = formatting(str(round_off(principal)))
     return principal
+
 import math
 def savings_calculator(present_value, future_value, term, rate, end_of_period=True):
     if not end_of_period:
@@ -45,46 +46,107 @@ def savings_calculator(present_value, future_value, term, rate, end_of_period=Tr
     ans = formatting(str(round_off(ans)))
     return ans 
 
-def files_innerjoin(filename1, filename2, common_column):
+def files_innerjoin(filename1, filename2, **kwargs):
     """inner join function on any number of keys"""
-    with open(filename1, 'r') as f1, open(filename2, 'r') as f2, open('results.csv', 'w') as f:
-        csv_reader_file1 = csv.reader(f1)
-        csv_reader_file2 = csv.reader(f2)
-        csv_writer_inner_join = csv.writer(f)
+    common_column = kwargs.get('common_column', [])
+    with open(filename1, 'r') as f:
+        file1_reader = csv.DictReader(f)
+        data1 = list(file1_reader)
+        header1 = list(data1[0].keys())
 
-        file1_data = [data for data in csv_reader_file1]
-        file2_data = [data for data in csv_reader_file2]
-        header1 = file1_data[0]
-        header2 = file2_data[0]
-        print(header1)
-        print(header2)
-        if common_column not in header1 or common_column not in header2:
-            return "r"
-        result_header = header1 + [header for header in header2 if header not in header1]
-        print(result_header)
-        csv_writer_inner_join.writerow(result_header)
-        
-        for i in range(0,len(header1)):
-            if header1[i] == common_column:
-                file1_index = i
-                break
-		
-        for i in range(0,len(header1)):
-            if header1[i] == common_column:
-                file2_index = i
-                break
-        final_data = []
-        for i in range(1,len(file1_data)):
-            for j in range(1, len(file2_data)): 
-                data1, data2, data= file1_data[i], file2_data[j], []
-                print(data1, data2)
-                if data1[file1_index] == data2[file2_index]:
-                    data = data1 + [i for i in data2 if i not in data1]
-                if data not in final_data:
-                    final_data.append(data)
-        
-        csv_writer_inner_join.writerows(final_data)
+    with open(filename2, 'r') as f:
+        file2_reader = csv.DictReader(f)
+        data2 = list(file2_reader)
+        header2 = list(data2[0].keys())
 
+    if all(bool(x not in header1 and x not in header2) for x in common_column):
+        return 
+    
+    result_header = header1 + [x for x in header2 if x not in header1]
+    final_data = []
+    for current_data1 in data1:
+        for current_data2 in data2:
+            if all(current_data1[x] == current_data2[x] for x in common_column):
+                current_data1.update(current_data2)
+                final_data.append(current_data1)
+    with open('results.csv', 'w') as f:
+        result_writer = csv.DictWriter(f, fieldnames = result_header)
+        result_writer.writeheader()
+        result_writer.writerows(final_data)
+
+def files_leftouterjoin(filename1, filename2, **kwargs):
+    """left outer join function on any number of keys"""
+    common_column = kwargs.get('common_column', [])
+    with open(filename1, 'r') as f:
+        file1_reader = csv.DictReader(f)
+        data1 = list(file1_reader)
+        header1 = list(data1[0].keys())
+
+    with open(filename2, 'r') as f:
+        file2_reader = csv.DictReader(f)
+        data2 = list(file2_reader)
+        header2 = list(data2[0].keys())
+
+    if all(bool(x not in header1 and x not in header2) for x in common_column):
+        with open('results.csv', 'w') as f:
+            result_writer = csv.DictWriter(f, fieldnames = header1)
+            result_writer.writeheader()
+            result_writer.writerows(data1)
+
+    
+    result_header = header1 + [x for x in header2 if x not in header1]
+    final_data = []
+    for current_data1 in data1:
+        match = 0
+        for current_data2 in data2:
+            if all(current_data1[x] == current_data2[x] for x in common_column):
+                current_data1.update(current_data2)
+                final_data.append(current_data1)
+                match += 1
+        if match == 0:
+            final_data.append(current_data1)
+
+    with open('results.csv', 'w') as f:
+        result_writer = csv.DictWriter(f, fieldnames = result_header)
+        result_writer.writeheader()
+        result_writer.writerows(final_data)
+
+def files_rightouterjoin(filename1, filename2, **kwargs):
+    """right outer join function on any number of keys"""
+    common_column = kwargs.get('common_column', [])
+    with open(filename1, 'r') as f:
+        file1_reader = csv.DictReader(f)
+        data1 = list(file1_reader)
+        header1 = list(data1[0].keys())
+
+    with open(filename2, 'r') as f:
+        file2_reader = csv.DictReader(f)
+        data2 = list(file2_reader)
+        header2 = list(data2[0].keys())
+
+    if all(bool(x not in header1 and x not in header2) for x in common_column):
+        with open('results.csv', 'w') as f:
+            result_writer = csv.DictWriter(f, fieldnames = header1)
+            result_writer.writeheader()
+            result_writer.writerows(data1)
+
+    
+    result_header = header1 + [x for x in header2 if x not in header1]
+    final_data = []
+    for current_data1 in data1:
+        match = 0
+        for current_data2 in data2:
+            if all(current_data1[x] == current_data2[x] for x in common_column):
+                current_data1.update(current_data2)
+                final_data.append(current_data1)
+                match += 1
+        if match == 0:
+            final_data.append(current_data2)
+
+    with open('results.csv', 'w') as f:
+        result_writer = csv.DictWriter(f, fieldnames = result_header)
+        result_writer.writeheader()
+        result_writer.writerows(final_data)
 
 def list_to_dict(data: list):
     """List of dicts to dict of lists"""
@@ -98,25 +160,46 @@ def list_to_dict(data: list):
     return ans
 
 def dict_to_list(data: dict):
+    """Convert a dict of lists to a list of dicts"""
     ans = []
-    counter = 1
-    while counter == 1:
-        counter = 0
-        temp = {}
-        for key,value in data.items():
-            if value:
-                counter = 1
-            temp[key] = value[0]
-            if data[key]:
-                data[key].pop()
-        ans.append(temp)
+    keys = list(data.keys())
+    values_lists = list(data.values())
+
+    for combination in zip(*values_lists):
+        dict_item = dict(zip(keys, combination))
+        ans.append(dict_item)
+
     return ans
+
+def split_file(filename, split_cols: list):
+    """plit a large csv file on values of one or more columns into multiple files"""
+    with open(filename, 'r') as f:
+        csv_reader = csv.DictReader(f)
+        data = list(csv_reader)
+        header = data[0].keys()
+    
+    if all(False if x in header else True for x in split_cols):
+        return
+    for x in data:
+        file_name = ""
+        for i in split_cols:
+            file_name += str(x[i])
+        file_name += '.csv'
+        with open(file_name, 'a') as f:
+            file_writer = csv.DictWriter(f, fieldnames=header)
+            if f.tell() == 0:
+                    file_writer.writeheader()
+            file_writer.writerow(x)
 
 if __name__ == "__main__":
     print(simple_interest(123456, 23, 0.08))
     print(compound_interest(123456, 23, 0.08))
     print(compound_interest_with_payments(0, 368970.52, 35, 0.10))
     print(savings_calculator(0, 1e8, 35, 0.10))
-    # print(files_innerjoin('file1.csv', 'file2.csv', 'Name'))
+    files_innerjoin('file1.csv', 'file2.csv', common_column = ['Name'])
+    files_leftouterjoin('file1.csv', 'file2.csv', common_column = ['Name'])
+    files_rightouterjoin('file1.csv', 'file2.csv', common_column = ['Name'])
     print(list_to_dict([{"name": "a", "age": 21}, {"name": "b", "age": 43}]))
     print(dict_to_list({"name": ["a", "b"], "age": [21,43]}))
+    split_file('file1.csv', ['Name', 'Age'])
+    
